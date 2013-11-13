45d9025f23cf84152a41e76654b92aa71b33ef1a
renjessp <renjessp@aol.com>
2013-11-13 17:57:54 -0500

    First commit

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Buxton Sketch", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(136, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 148);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // displayButton
+            // 
+            this.displayButton.AutoSize = true;
+            this.displayButton.BackColor = System.Drawing.Color.Transparent;
+            this.displayButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.displayButton.Font = new System.Drawing.Font("Gisha", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayButton.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.displayButton.Location = new System.Drawing.Point(260, 189);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(312, 74);
+            this.displayButton.TabIndex = 1;
+            this.displayButton.Text = "Click Here to Display Message";
+            this.displayButton.UseVisualStyleBackColor = false;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(688, 460);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "by Renan Ueno";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(815, 513);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hello World Assignment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
